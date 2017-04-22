0f328ba0b9b94e40792c858482a1d1264a0067b2
Tyrone <tyrone@ubuntu.ubuntu-domain>
2017-04-22 16:09:38 -0700

    added secrets to .gitignore, precompiled assets

--- config/initializers/inline_svg.rb
+++
@@ -1,8 +0,0 @@
-InlineSvg.configure do |config|
-  config.asset_file = InlineSvg::CachedAssetFile.new(
-    paths: [
-      "#{Rails.root}/app/assets/images/"
-    ],
-    filters: /\.svg/
-  )
-end
